e26ee949dd6ce130a6bf0a550e74f6aad1305679
Lukasz Mowinski <lmowinski@deloittece.com>
2023-01-26 15:14:15 +0100

    Initial commit

---
+++ src/main/java/com/example/pdrive/PDriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.pdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pdrive/PDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
