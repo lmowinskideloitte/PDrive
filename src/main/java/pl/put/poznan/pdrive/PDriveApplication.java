b949bba055e7090ca54eaee3919419c8f733a5c5
Lukasz Mowinski <lmowinski@deloittece.com>
2023-01-26 17:15:10 +0100

    fix: groupId changed

--- src/main/java/pl/put/poznan/pdrive/PDriveApplication.java
+++ src/main/java/pl/put/poznan/pdrive/PDriveApplication.java
@@ -1,4 +1,4 @@
-package com.example.pdrive;
+package pl.put.poznan.pdrive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/pl/put/poznan/pdrive/PDriveApplicationTests.java
+++ src/test/java/pl/put/poznan/pdrive/PDriveApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.pdrive;
+package pl.put.poznan.pdrive;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
