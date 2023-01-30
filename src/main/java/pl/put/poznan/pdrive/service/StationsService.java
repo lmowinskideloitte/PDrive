60469564c2c7fcdf1729823b47253614c6171cb1
Lukasz Mowinski <lmowinski@deloittece.com>
2023-01-30 13:17:58 +0100

    feat: addStation

--- src/main/java/pl/put/poznan/pdrive/service/StationsService.java
+++ src/main/java/pl/put/poznan/pdrive/service/StationsService.java
@@ -1,5 +1,6 @@
 package pl.put.poznan.pdrive.service;
 
+import pl.put.poznan.pdrive.entity.Location;
 import pl.put.poznan.pdrive.entity.Station;
 
 import java.util.List;
@@ -7,4 +8,6 @@ import java.util.List;
 public interface StationsService {
 
     List<Station> getAllStations();
+
+    Station addStation(Location location, String address);
 }
--- src/main/java/pl/put/poznan/pdrive/service/impl/StationsServiceImpl.java
+++ src/main/java/pl/put/poznan/pdrive/service/impl/StationsServiceImpl.java
@@ -2,7 +2,9 @@ package pl.put.poznan.pdrive.service.impl;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.put.poznan.pdrive.entity.Location;
 import pl.put.poznan.pdrive.entity.Station;
+import pl.put.poznan.pdrive.repository.LocationRepository;
 import pl.put.poznan.pdrive.repository.StationRepository;
 import pl.put.poznan.pdrive.service.StationsService;
 
@@ -13,9 +15,20 @@ import java.util.List;
 public class StationsServiceImpl implements StationsService {
 
     private final StationRepository stationRepository;
+    private final LocationRepository locationRepository;
 
     @Override
     public List<Station> getAllStations() {
         return stationRepository.findAll();
+    }
+
+    @Override
+    public Station addStation(Location location, String address) {
+        Location found = locationRepository.findById(location.getId()).orElseThrow();
+        Station station = new Station();
+        station.setLocation(found);
+        station.setAddress(address);
+
+        return stationRepository.save(station);
     }
 }
